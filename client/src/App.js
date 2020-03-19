6a518eca11ff7d9056858fd9122d76fb937856d3
Jennifer <jennwong1009@gmail.com>
2020-03-18 18:29:12 -0700

    includes client folder, creates appNavbar component

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import AppNavbar from './components/AppNavbar'
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar />
+    </div>
+  );
+}
+
+export default App;
