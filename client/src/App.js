5f694e8fc683cdb71bb8e0d06ed4c7f12a929dd4
Jennifer <jennwong1009@gmail.com>
2020-03-18 19:01:38 -0700

    adds shopping list component

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import AppNavbar from './components/AppNavbar'
+import ShoppingList from './components/ShoppingList';
 
 function App() {
   return (
     <div className="App">
       <AppNavbar />
+      <ShoppingList />
     </div>
   );
 }
