c4a132c238961f560318f6811cb393f3f0427c57
Jennifer <jennwong1009@gmail.com>
2020-03-28 21:41:23 -0700

    config

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,6 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://admin:d1ig8TZFWQXqOx77@shopping-list-1lkcz.mongodb.net/test?retryWrites=true&w=majority'
+if(process.env.NODE_ENV === "production") {
+    module.exports = require("./keys_prod");
+} else {
+    module.exports = require("./keys_dev");
 }
+
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-
 const items = require('./routes/api/items')
 
 const app = express();
